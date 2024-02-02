dd52fdcb2536f0aa92a1142418e054219dea2418
andrius-butent <andrius.antipovas@gmail.com>
2024-02-02 13:49:13 +0200

    Added integration tests

---
+++ src/test/java/com/spring/andrius/msscbreweryclient/client/BreweryClientTest.java
@@ -0,0 +1,50 @@
+package com.spring.andrius.msscbreweryclient.client;
+
+import com.spring.andrius.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+
+        BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+
+    @Test
+    void postNewBeer() {
+        // given
+        BeerDto beerDto = BeerDto.builder().beerName("Calsberg").build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+
+        System.out.println(uri.toString());
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateBeer() {
+        // given
+        BeerDto beerDto = BeerDto.builder().beerName("Corona Extra").build();
+
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+}
